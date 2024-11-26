d681cff770682e3f5f73231b79c7a8c015dc82d8
unknown <danil.perfiliv@gmail.com>
2024-11-26 19:31:23 +0700

    fixes: logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,14 +4,19 @@ import "./globals.css";
 import Head from "next/head";
 import Script from "next/script";
 
-
 import { Montserrat } from "next/font/google"
-
+import { Leckerli_One } from "next/font/google"
 
 const montserrat = Montserrat ({
   weight: ['100', '300', '500', '700'],
   subsets: ['cyrillic'],
   variable: "--font-montserrat"
+})
+
+const font = Leckerli_One ({
+  weight: ['400'],
+  subsets: ['latin'],
+  variable: "--font"
 })
 
 export const metadata: Metadata = {
@@ -27,20 +32,17 @@ export default function RootLayout({
   return (
     <html lang="ru">
     
-   
       <head>
-        
         
         <script src="https://aframe.io/releases/1.0.4/aframe.min.js" />
         <script src="https://raw.githack.com/AR-js-org/AR.js/master/aframe/build/aframe-ar.js" />
         <script src="./scripts/aframe-particle-system-component.js" />
         <script src="./scripts/gesture-detector.js" />
         <script src="./scripts/gesture-handler.js" />
-
         
       </head>
     
-      <body className={`${montserrat.variable}`}>
+      <body className={`${montserrat.variable} ${font.variable}`}>
 
         {children}
 
