c396c7b5a5df69e1a34bbd1653419d57ff82e7a5
unknown <danil.perfiliv@gmail.com>
2024-11-26 19:18:45 +0700

    fixes: about

--- src/app/ar-markers/model.tsx
+++ src/app/ar-markers/model.tsx
@@ -26,7 +26,7 @@ export default function Model({ url }: {url: string}) {
 
     return (
         <Canvas style={{ width: '100%', height: '100%', borderRadius: "20px" }} camera={{ position: [3, 1, 1], fov: 75 }}>
-            <ambientLight intensity={4} />
+            <ambientLight intensity={2.5} />
             <pointLight position={[10, 10, 10]}/>
             
             <Suspense fallback={<Loader />}>
