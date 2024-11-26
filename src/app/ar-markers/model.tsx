07f618ca50234d73769908da1ae8b50c7246a195
unknown <danil.perfiliv@gmail.com>
2024-11-26 19:50:46 +0700

    models animation

--- src/app/ar-markers/model.tsx
+++ src/app/ar-markers/model.tsx
@@ -1,10 +1,10 @@
 import styles from "./page.module.css";
 
-import React, { Suspense } from 'react';
-import { Canvas } from '@react-three/fiber';
+import React, { Suspense, useRef } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
 import { OrbitControls, useGLTF } from '@react-three/drei';
 import { Html } from '@react-three/drei';
-
+import * as THREE from 'three';
 
 function Loader() {
     return (
@@ -15,28 +15,35 @@ function Loader() {
 }
 
 function ModelViewer({ modelPath }: { modelPath: string }) {
-    const gltf = useGLTF(modelPath, true); 
+    const gltf = useGLTF(modelPath, true);
+    const meshRef = useRef<THREE.Object3D>(null);
 
-    return <primitive object={gltf.scene} position={[0, -1, 0]} />;
+    useFrame(() => {
+        if (meshRef.current) {
+            meshRef.current.rotation.y += 0.005;
+        }
+    });
+
+    return <primitive ref={meshRef} object={gltf.scene} position={[0, -1, 0]} />;
 }
 
-export default function Model({ url }: {url: string}) {
+export default function Model({ url }: { url: string }) {
 
-    const modelPath = url;  
+    const modelPath = url;
 
     return (
-        <Canvas style={{ width: '100%', height: '100%', borderRadius: "20px" }} camera={{ position: [3, 1, 1], fov: 75 }}>
+        <Canvas style={{ width: '100%', height: '100%', borderRadius: "20px" }} camera={{ position: [2, 1, 1], fov: 75 }}>
             <ambientLight intensity={2.5} />
-            <pointLight position={[10, 10, 10]}/>
-            
+            <pointLight position={[10, 10, 10]} />
+
             <Suspense fallback={<Loader />}>
                 <ModelViewer modelPath={modelPath} />
             </Suspense>
 
-            <OrbitControls 
-            enablePan={false}
-                minDistance={2} 
-                maxDistance={5} 
+            <OrbitControls
+                enablePan={false}
+                minDistance={2}
+                maxDistance={5}
             />
         </Canvas>
     );
