10a5d5db0fec3e4eaf10166707b1b2e4482e4734
unknown <danil.perfiliv@gmail.com>
2024-11-30 19:53:18 +0700

    scan page window issue

--- src/app/scan/page.tsx
+++ src/app/scan/page.tsx
@@ -6,29 +6,26 @@ import ARScene from "./ARScene"
 
 import Button from "../components/button/button"
 
+import { useRouter } from "next/navigation";
+
 export default function Scan() {
+  const router = useRouter();
+
+  const goToMainPage = () => {
+    router.back();
+  };
+
+  return (
+
+    <div className={styles.scan}>
+      <ARScene />
+
+      <div className={styles.panel}>
+        <Button isRound={true} imgSrc="/images/icons/ui/close-black.png" onClick={goToMainPage} />
+      </div>
+
+    </div>
 
 
-    const goToMainPage = () => {
-      if (typeof window !== "undefined") {
-        window.location.href = "/";
-      }
-    };
-
-    
-  
-  
-    return (
-
-      <div className={styles.scan}>
-            <ARScene />
-            
-            <div className={styles.panel}>
-                <Button isRound={true} imgSrc="/images/icons/ui/close-black.png" onClick={goToMainPage}/>
-            </div>
-            
-        </div>
-      
-        
-    )
+  )
 }
