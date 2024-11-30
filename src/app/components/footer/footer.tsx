5c19692cfe41c4e6fe18980904c823775136a9dc
unknown <danil.perfiliv@gmail.com>
2024-11-30 12:21:23 +0700

    sibfu logo

--- src/app/components/footer/footer.tsx
+++ src/app/components/footer/footer.tsx
@@ -1,5 +1,6 @@
 import styles from "./footer.module.css"
 import Image from "next/image"
+import Link from "next/link"
 
 import Button from "../button/button"
 
@@ -11,8 +12,10 @@ export default function Footer() {
             </p>
             <div className={styles.socials}>
                 {/* <Button isRound={true} imgSrc="/images/icons/ui/vk.svg" href="https://vk.com/"/> */}
+                <Link className={styles.wrapper} href="/">
+                    <Image className={styles.sibfu} src="/images/icons/ui/sibfu.svg" alt="" fill />
+                </Link>
                 <Button isRound={true} imgSrc="/images/icons/ui/tg.svg" href="https://web.telegram.org/"/>
-                
             </div>
         </div>
     )
--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -6,16 +6,9 @@ import Link from "next/link"
 export default function Header() {
     return (
         <div className={styles.header}>
-            <div className={styles.nav}>
-                <Link className={styles.wrapper} href="/">
-                    <Image className={styles.sibfu} src="/images/icons/ui/sibfu.svg" alt="" fill />
-                </Link>
-                <Link href="/">
-                    <span className="text-logotype">ar-postcards</span>
-                </Link>
-
-
-            </div>
+            <Link href="/">
+                <span className="text-logotype">ar-postcards</span>
+            </Link>
             <div className={styles.nav}>
                 <Button isScalable={true} text="Помощь" imgSrc="/images/icons/ui/question.svg" href="/help" />
                 <Button isScalable={true} text="О Сервисе" imgSrc="/images/icons/ui/exclamation.svg" href="/about" />
