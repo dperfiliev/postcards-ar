a1a11eaa2e18c7adeea1cc4f898c188328b99a62
unknown <danil.perfiliv@gmail.com>
2024-11-30 20:17:51 +0700

    meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,12 @@ export default function RootLayout({
     <html lang="ru">
       <head>
 
-
+      <meta
+        aframe-injected=""
+        name="viewport"
+        content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no, user-scalable=no, minimal-ui, viewport-fit=cover"
+      />
+      
         <script src="/scripts/aframe.min.js" />
         <script src="/scripts/aframe-ar.js" />
         <script src="/scripts/aframe-particle-system-component.js" />
--- src/app/scan/page.tsx
+++ src/app/scan/page.tsx
@@ -1,37 +1,30 @@
 "use client"
 
 import styles from "./page.module.css"
-import dynamic from "next/dynamic";
-//import ARScene from "./ARScene"
+
+import ARScene from "./ARScene"
 
 import Button from "../components/button/button"
 
 import { useRouter } from "next/navigation";
 
-import { useEffect } from "react";
-
 export default function Scan() {
   const router = useRouter();
-
-  useEffect(() => {
-    // Any code that accesses the window object should go here
-  }, []);
 
   const goToMainPage = () => {
     router.back();
   };
-  const ClientOnlyComponent = dynamic(() => import("./ARScene"), { ssr: false });
+
   return (
 
     <div className={styles.scan}>
-      <ClientOnlyComponent />
+      <ARScene />
 
       <div className={styles.panel}>
         <Button isRound={true} imgSrc="/images/icons/ui/close-black.png" onClick={goToMainPage} />
       </div>
 
     </div>
-
-
+    
   )
 }
