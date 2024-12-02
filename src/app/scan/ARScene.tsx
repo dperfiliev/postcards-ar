18562dc1226a7a5fc25bbb1f669a8dcf5c20e992
unknown <danil.perfiliv@gmail.com>
2024-12-02 13:39:25 +0700

    scan

--- src/app/scan/ARScene.tsx
+++ src/app/scan/ARScene.tsx
@@ -85,7 +85,7 @@ const ARScene = () => {
             cursor="fuse: false; rayOrigin: mouse;"
             detectionMode="color"
             maxDetectionRate="30"
-            minConfidence="0.6"
+            minConfidence="0.7"
           >
             <a-entity
               gltf-model={model}
