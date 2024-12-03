b47668ef39c8d7ad6f4c55b2997f8ac589469007
unknown <danil.perfiliv@gmail.com>
2024-12-03 12:21:35 +0700

    footer

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -6,9 +6,14 @@ import Link from "next/link"
 export default function Header() {
     return (
         <div className={styles.header}>
-            <Link href="/">
-                <span className="text-logotype">ar-postcards</span>
-            </Link>
+            <div className={styles.logos}>
+                <Link className={styles.wrapper} href="https://www.sfu-kras.ru/">
+                    <Image className={styles.sibfu} src="/images/icons/ui/sibfu.png" alt="" fill sizes="40px" />
+                </Link>
+                <Link href="/">
+                    <span className="text-logotype">ar-postcards</span>
+                </Link>
+            </div>
             <div className={styles.nav}>
                 <Button isScalable={true} text="Помощь" imgSrc="/images/icons/ui/question.png" href="/help" />
                 <Button isScalable={true} text="О Сервисе" imgSrc="/images/icons/ui/exclamation.png" href="/about" />
